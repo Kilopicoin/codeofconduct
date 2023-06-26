a7f691ea4a0c821ca5468fc0bd846b4cd01f51ad
Cem Tosun <134544650+Kilopi3142@users.noreply.github.com>
2023-06-26 19:37:15 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,80 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+	  <div className="Appx">
+	  
+	  <h1>Kilopi code of conduct</h1>
+
+	   <h2>Introduction</h2>
+	   <h3>Diversity and Collective Intelligence are 2 key factors of Kilopi project.</h3>
+	   <h3>Kilopi Main Team and the community have some serious responsibilities about this code of conduct.</h3>
+	   <h3>Every team member has to understand the importance of diversity, listen to others and discuss the problems within the logical understanding and behaviour.</h3>
+	   <h3>Every community member has to follow the strict rules of the Kilopi project. Otherwise, team members have to detect the toxic behaviour that is damaging the project and  
+	   follow the necessary steps to clean the project.</h3>
+	   
+	    <h2>Commitment to the community</h2>
+	   
+	   <h3>Kilopi project team commits to interact with the community in ways that is open, welcoming, diverse, inclusive, and healthy, regardless of age, body size, visible or invisible disability, ethnicity, sex characteristics, 
+	   gender identity and expression, level of experience, education, socio-economic status, nationality, personal appearance, race, religion, or sexual identity and orientation.</h3>
+
+	   
+	   <h2>Community standards</h2>
+	   <h3>Examples of behavior that contributes to a positive environment for community include:</h3>
+	   <h3>Making education a priority, both the team and the entire community</h3>
+	   <h3>Demonstrating empathy and kindness toward other people</h3>
+	   <h3>Being respectful of differing opinions, viewpoints, and experiences</h3>
+	   <h3>Giving and gracefully accepting constructive feedback</h3>
+	   <h3>Accepting responsibility and apologizing to those affected by our mistakes, and learning from the experience</h3>
+	   <h3>Focusing on what is best not just for us as individuals, but for the overall community</h3>
+	   <h3>Examples of unacceptable behavior include:</h3>
+	   <h3>The use of sexualized language or imagery, and sexual attention or advances of any kind</h3>
+	   <h3>Trolling, insulting or derogatory comments, and personal or political attacks</h3>
+	   <h3>Public or private harassment</h3>
+	   <h3>Publishing others’ private information, such as a physical or email address, without their explicit consent</h3>
+	   <h3>Discussing the price of a token or asset, or giving any type of financial advice</h3>
+	   <h3>Other conduct which could reasonably be considered inappropriate in a professional setting</h3>
+	   
+	    <h2>Enforcing</h2>
+	   <h3>Community Leaders, Ambassadors and moderators, are responsible for clarifying and 
+	   enforcing our standards of acceptable behavior and will take appropriate and fair corrective action in response to any behavior that they deem inappropriate, threatening, offensive, or harmful.</h3>
+	   
+	  <h2>Community impact and enforcement guidelines</h2>
+	  <h3>Responsible team members will follow these Community Impact and Enforcement Guidelines listed below, 
+	  in determining the consequences for any action they deem in violation of this Code of Conduct:</h3>
+	  <h3>1. Correction</h3>
+	  <h3>Community Impact: A single use of inappropriate language or other behavior deemed unprofessional or unwelcome in the community.</h3>
+	  
+	  <h3>Consequence: Deletion or removal of the post in question, in addition to communicating which rule was violated and how to avoid such violations in the future. If the action was in person, 
+	  direct communication with that individual to identify which rule was violated and how to avoid such violations in the future.</h3>
+
+	   <h3>2. Warning</h3>
+	   <h3>Community Impact: A violation through a single incident or series of actions.</h3>
+	   
+	   <h3>Consequence: A warning with consequences for continued behavior. No interaction with the people involved, including unsolicited interaction with those enforcing the Code of Conduct, for a specified period of time. 
+	   This includes avoiding interactions in community spaces as well as external channels like Telegram, Discord,  etc. Violating these terms may lead to a temporary or permanent ban.</h3>
+	   
+	  <h3>3. Temporary Ban</h3>
+	  
+	  <h3>Community Impact: A serious violation of community standards, including sustained inappropriate behavior.</h3>
+	  
+	  <h3>Consequence: A temporary ban from any sort of interaction or public communication with the Community for a specified period of time. No public or private interaction with the people involved, 
+	  including unsolicited interaction with those enforcing the Code of Conduct, is allowed during this period. Violating these terms may lead to a permanent ban.</h3>
+	  
+	  <h3>4. Permanent Ban</h3>
+	  
+	  <h3>Community Impact: Demonstrating a pattern of violation of community standards, 
+	  including sustained inappropriate behavior, harassment of an individual, or aggression toward or disparagement of classes of individuals.</h3>
+	  
+	  <h3>Consequence: Permanent removal from any sort of interaction or public communication with the Community for a specified period of time. If an individual has alternate accounts those may be removed as well. 
+	  No public or private interaction with the people involved, including unsolicited interaction with those enforcing the Code of Conduct, is allowed.</h3>
+	  
+	  </div>
+    </div>
+  );
+}
+
+export default App;
